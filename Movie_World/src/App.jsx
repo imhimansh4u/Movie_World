8890a2153fbb3614278291de8896035514948c2b
Himanshu singh <himanshusinghrajput1107@gmail.com>
2025-11-16 18:33:56 +0530

    added new Routes

--- Movie_World/src/App.jsx
+++ Movie_World/src/App.jsx
@@ -4,18 +4,21 @@ import { Routes , Route} from "react-router-dom";
 import "./css/App.css"
 import Favorites from "./pages/Favorites.jsx";
 import NavBar from "./components/NavBar.jsx";
+import { MovieProvider } from "./contexts/MovieContexts.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
